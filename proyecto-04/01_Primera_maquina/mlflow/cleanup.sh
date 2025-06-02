226a750ff6683e8ec310805d7f9be3b78bdb986f
Miguel Zuniga <miguelh.zuniga@javeriana.edu.co>
2025-06-02 02:55:30 -0500

    ajuste git action

--- proyecto-04/01_Primera_maquina/mlflow/cleanup.sh
+++ proyecto-04/01_Primera_maquina/mlflow/cleanup.sh
@@ -2,25 +2,18 @@
 
 echo "Eliminando despliegue completo de recursos en mlops-project..."
 
-# Borrar los deployments en el namespace mlops-project
 kubectl delete deployments --all -n mlops-project
 
-# Borrar los services en el namespace mlops-project
 kubectl delete services --all -n mlops-project
 
-# Borrar los pods en el namespace mlops-project (por si hay pods huérfanos)
 kubectl delete pods --all -n mlops-project
 
-# Borrar los PersistentVolumeClaims si existen (eliminar almacenamiento persistente)
 kubectl delete pvc --all -n mlops-project
 
-# Borrar los jobs (como el job de inicialización de MinIO)
 kubectl delete jobs --all -n mlops-project
 
-# Borrar ConfigMaps si existen
 kubectl delete configmaps --all -n mlops-project
 
-# Borrar los secretos si hay alguno (usar con cuidado en entornos productivos)
 kubectl delete secrets --all -n mlops-project
 
 #Forzar
