d93e1b852523cec5dec9f5e4b08957e567861896
Miguel Zuniga <miguelh.zuniga@javeriana.edu.co>
2025-06-01 22:57:07 -0500

    ajuste path argo cd

---
+++ proyecto-04/03_Tercera_maquina/stop_port_forwards.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -d "port_forward_logs" ]; then
+    for pid_file in port_forward_logs/*.pid; do
+        if [ -f "$pid_file" ]; then
+            PID=$(cat "$pid_file")
+            SERVICE=$(basename "$pid_file" .pid)
+            if ps -p $PID > /dev/null; then
+                echo "Deteniendo port-forward para $SERVICE (PID: $PID)..."
+                kill $PID
+            else
+                echo "El proceso de port-forward para $SERVICE ya no está en ejecución"
+            fi
+        fi
+    done
+    echo "Todos los port-forwards detenidos"
+else
+    echo "No se encontró información de port-forwarding, por favor validar"
+fi
